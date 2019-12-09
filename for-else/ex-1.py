ed010b0420cc041e2f8a1b6da3b76e0454b7792e
tohfaakib <tohfaakib@my.smccd.edu>
2019-12-09 17:17:06 +0600

    for else

---
+++ for-else/ex-1.py
@@ -0,0 +1,9 @@
+# finding prime number
+
+for n in range(2, 100):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n / x)
+            break
+    else:
+        print(n, 'is a prime number')
