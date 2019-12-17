c448efee4af7baebcecc56ac88ba87330dcb6d6d
tohfaakib <tohfaakib@my.smccd.edu>
2019-12-17 17:05:44 +0600

    function cache

---
+++ function cache/ex.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=32)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+# fib.cache_clear()
+print([fib(n) for n in range(10)])
